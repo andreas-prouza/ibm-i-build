86f8ee3171a7262ec8eb5d927d21c1554dbe9cbf
Andreas Prouza <andreas@prouza.at>
2023-08-18 21:48:36 +0200

    Object list & build script created

--- build/compile.sh
+++ build/compile.sh
@@ -9,10 +9,14 @@ mkdir -p ./build/prouzalib2
 #  
 )
 ( liblist -a PROUZAT1 2> /dev/null; cl  -v  "CRTRPGMOD MODULE("PROUZAT2/test1") SRCSTMF('./prouzalib2//qrpglesrc/"test1".rpgle') DBGVIEW(*SOURCE) REPLACE(*YES) TGTCCSID(*JOB) INCDIR('./prouzalib2/')"    2>> './logs/prouzalib2/test1.rpgle.pgm.error.log'  >> './logs/prouzalib2/test1.rpgle.pgm.splf.log' ; cl -v  "DSPJOBLOG" >> './logs/prouzalib2/test1.rpgle.pgm.joblog.log' )
+if [ -s './logs/prouzalib2/test1.rpgle.pgm.error.log' ]; then
+echo -e '\033[101;31mfailed: prouzalib2//qrpglesrc/test1.rpgle\033[0m' 1>&2
+exit 1
+fi
 ( liblist -a PROUZAT1 2> /dev/null; cl  -v  "CRTPGM PGM("PROUZAT2/test1") ACTGRP(PROUZAGRP) REPLACE(*YES) TGTRLS(*CURRENT) STGMDL(*SNGLVL) DETAIL(*BASIC) BNDDIR(*LIBL/PROUZADIR)"    2>> './logs/prouzalib2/test1.rpgle.pgm.error.log'  >> './logs/prouzalib2/test1.rpgle.pgm.splf.log' ; cl -v  "DSPJOBLOG" >> './logs/prouzalib2/test1.rpgle.pgm.joblog.log' )
 if [ -s './logs/prouzalib2/test1.rpgle.pgm.error.log' ]; then
 echo -e '\033[101;31mfailed: prouzalib2//qrpglesrc/test1.rpgle\033[0m' 1>&2
-exit
+exit 1
 fi && touch './build/prouzalib2/test1.rpgle.pgm'; date > ./build/prouzalib2/test1.rpgle.pgm
 cat ./logs/*.error.log 2> /dev/null | true
 echo -e "\033[102;97mCompile complete\033[0m"
